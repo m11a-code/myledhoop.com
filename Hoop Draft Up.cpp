06e9ad776d2b4cb482d32680aa0ed1e610d91659
Matthew Ibarra <msibarr@gmail.com>
2015-02-01 12:08:27 -0500

    Push of code - 02.01.2015
    Push of draft code for wireless, stable code, libraries, and LED light
    strip test code.

---
+++ Hoop Draft Up.cpp
@@ -0,0 +1,124 @@
+// http://www.myledhoop.com
+// LED Hula Hoop
+// Wireless remote control with nRF24L01 module
+// The 'hoop' is controlled by the "Remote Arduino Remote Control"
+
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+// nRF24L01 radio
+RF24 radio(48,49);    // What is the 48 and 49? Need to look into this.
+const uint64_t pipes[2] = { 
+  0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+
+// Timer
+#define runEvery(t) for (static typeof(t) _lasttime;(typeof(t))((typeof(t))millis() - _lasttime) > (t);_lasttime += (t))
+
+// More output pins
+// const int redLed = 34; // low battery led
+// const int buzzer = 35; // buzzer
+// const int orangeLeds = 38; // orange leds
+// const int headlight = 39; // white leds
+// const int blueLed = 40;   // blue led = wireless link down
+
+// Structs for communication
+typedef struct{
+  // int X;
+  // int Y;
+  // int Z;
+  int A;
+  // int B;
+  // int C;
+  // int D;
+}
+struct1_t;
+
+// typedef struct{
+//   float motors;
+//   float batRover;
+// }
+// struct2_t;
+// struct2_t rover;
+
+typedef struct{
+  int led_mode;   // Just ideas for now.
+  int isOK;       // Just ideas for now.
+}
+struct2_t;
+
+struct1_t remote;
+struct2_t hoop;
+
+// Other stuff...
+// byte current;
+// byte battery;
+// byte orangeLedState;
+unsigned long startTime;
+unsigned long endTime;
+int time;
+
+void setup()
+{
+  Serial.begin(115200);   // Need to see why this number is bigger than the remote's baud rate...
+  // Setup radio
+  radio.begin();
+  radio.setPALevel( RF24_PA_MAX ) ; 
+  radio.setDataRate(RF24_1MBPS);
+  //radio.setDataRate(RF24_250KBPS);
+  //radio.enableDynamicPayloads() ;
+  //radio.setAutoAck(true);
+  //radio.setPayloadSize(14);
+  radio.openWritingPipe(pipes[1]);
+  radio.openReadingPipe(1,pipes[0]);
+  radio.startListening();
+
+}
+
+void loop(void)
+{
+  startTime = millis();
+
+  // Radio 
+  runEvery(20)     // running this code too often leads to dropped packets
+  {
+    radio.stopListening();
+    bool ok = radio.write( &rover, sizeof(rover) );
+    radio.startListening();
+
+    unsigned long started_waiting_at = millis();
+    bool timeout = false;
+    while ( ! radio.available() && ! timeout )
+      //if (millis() - started_waiting_at > 1+(radio.getMaxTimeout()/1000) )
+      if (millis() - started_waiting_at > 200 )
+        timeout = true;
+
+    if ( timeout )          // no wireless connection
+    {
+      // digitalWrite(blueLed, HIGH);
+      stopped();
+      Serial.println("Failed, response timed out.");
+    }
+    else
+    {
+      // digitalWrite(blueLed, LOW);
+      radio.read( &remote, sizeof(remote) );
+    }
+  }
+  // end of radio stuff
+
+  endTime = millis();
+  time = endTime - startTime;
+
+}
+
+void stopped()
+{
+  // Need to add here code that turns off the hoop.
+}
+
+// Lots of work to do here!
+
+
+
+
---
+++ Remote Draft Up.cpp
@@ -0,0 +1,111 @@
+// Stuff to use for remote (possibly)
+
+#include <SPI.h>	// Need to look up exactly what this is but whatevs for now.
+#include "nRF24L01.h"
+#include "RF24.h"
+// #include <Wire.h>	Not used now, but if we used an I2C communicating LCD display, this library is used to handle the I2C protocol communication.
+// #include <LiquidCrystal_I2C.h>	// The LCD display over I2C library.
+#include <Bounce.h>	// Helps with push buttons to avoid "bouncing" aka multiple low high reads (resulting in it reading multiple pushes) when the button was only pressed one time by the user.
+// The bounce may just be for pushing down the joystick on the dude's remote... Not something we need to worry about.
+
+#define runEvery(t) for (static typeof(t) _lasttime;(typeof(t))((typeof(t))millis() - _lasttime) > (t);_lasttime += (t))
+
+typedef struct{
+  // int X;
+  // int Y;
+  // int Z;
+  int A;
+  // int B;
+  // int C;
+  // int D;
+}
+struct1_t;
+
+typedef struct{
+  int led_mode;   // Just ideas for now.
+  int isOK;       // Just ideas for now.
+}
+struct2_t;
+
+struct1_t remote;
+struct2_t hoop;
+// struct2_t rover;
+
+RF24 radio(8,9);
+const uint64_t pipes[2] = { 
+  0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+
+const int aButton = 4;
+// const int bButton = 5;
+
+void setup()
+{
+  Serial.begin(57600);
+  pinMode(aButton , INPUT);
+  digitalWrite(aButton, HIGH);
+  // pinMode(bButton , INPUT);
+  // digitalWrite(bButton, HIGH);
+  radio.begin();
+  radio.setPALevel( RF24_PA_MAX ) ; 
+  //radio.setDataRate(RF24_250KBPS);
+  //radio.setAutoAck( true ) ;
+  radio.setDataRate(RF24_1MBPS);
+  //radio.setPayloadSize(14);
+  //radio.enableDynamicPayloads() ;
+  radio.openWritingPipe(pipes[0]);
+  radio.openReadingPipe(1,pipes[1]);
+  radio.startListening();
+}
+
+void loop(void)
+{
+  // analogRead(A3);
+  // batRemote = analogRead(A3) / 102.3;
+  analogRead(A0);
+  remote.X = analogRead(A0);
+  // analogRead(A1);
+  // remote.Y = analogRead(A1);
+  // remote.A = digitalRead(clawOpen);    
+  // remote.B = digitalRead(clawClose);
+  // remote.C = digitalRead(topButton);  
+  // remote.D = digitalRead(bottomButton);
+  
+  // Note: this may be for the joystick z axis push down and may not be necessary in our usage. 
+  // if ( bouncerz.update() ) {
+  //   if ( bouncerz.read() == LOW) {
+  //     if ( remote.Z == HIGH ) {
+  //       remote.Z = LOW;
+  //     } 
+  //     else {
+  //       remote.Z = HIGH;
+  //     }
+  //   }
+  // }
+
+  // radio stuff
+  if ( radio.available() )
+  {
+    bool done = false;
+    while (!done)
+    {
+      // done = radio.read( &rover, sizeof(rover) );
+      done = radio.read( &hoop, sizeof(hoop) );
+    }
+    radio.stopListening();
+    radio.write( &remote, sizeof(remote) );
+    radio.startListening();
+  }
+  // end of radio stuff
+
+  //lcd stuff
+  // runEvery(200)
+  // {
+  // lcd.setCursor(8,0);
+  // lcd.print(rover.motors);
+  // lcd.setCursor(13,2);
+  // lcd.print(batRemote);
+  // lcd.setCursor(12,3);
+  // lcd.print(rover.batRover);
+  // }
+  // end of lcd stuff
+}
